b0d0326f859b64dc5bd7053e41d6285aa906eb87
Oliynik Maksym <maksymoliynik@gmail.com>
2022-12-29 11:29:54 +0200

    bugfix and completed

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,23 +1,58 @@
 import {
-	addMovieToList,
-	createMarkup,
-	createStyle,
-	moviesList
-} from './dom.js';
+  addMovieToList,
+  createMarkup,
+  createStyle,
+  moviesList,
+  inputSearch,
+  triggerMode,
+  clearMoviesMarkup,
+} from "./dom.js";
 
 const getData = (url) => {
-	return fetch(url)
-	.then((res) => res.json())
-	.then((data) => data.Search);
-}
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data.Search);
+};
 
-const search = 'iron man';
+let searchLLast = null;
 
-getData(`https://www.omdbapi.com/?i=tt3896198&apikey=4d447f57&s=${search}`)
-.then(movies => movies.forEach(movie => addMovieToList(movie)))
-.catch(console.log);
+const debounce = (() => {
+  let timer = null;
+
+  return (cb, ms) => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+
+    timer = setTimeout(cb, ms);
+  };
+})();
+
+const inputSearchHandler = (e) => {
+  debounce(() => {
+    const searchString = e.target.value.trim();
+
+    if (
+      searchString &&
+      searchString.length > 3 &&
+      searchLLast !== searchString
+    ) {
+      if (!triggerMode) clearMoviesMarkup(moviesList);
+
+      getData(
+        `https://www.omdbapi.com/?i=tt3896198&apikey=4d447f57&s=${searchString}`
+      )
+        .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
+        .catch(console.log);
+    }
+
+    searchLLast = searchString;
+  }, 2000);
+};
 
 export const appInit = () => {
-	createStyle();
-	createMarkup();
-}
+  createStyle();
+  createMarkup();
+  inputSearch.addEventListener("keyup", inputSearchHandler);
+};
